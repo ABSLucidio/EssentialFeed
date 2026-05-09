9bb7738e1e4369bacbd3902ac3408a45c2429e3b
lucidiosouzaavanade <lucidiosouza.avanade@natura.net>
2026-05-09 12:33:59 -0300

    implement  urlsession scenarios

--- EssentialFeedTests/Feed API/XCTestCase+MemoryLeakTracking.swift
+++ EssentialFeedTests/Feed API/XCTestCase+MemoryLeakTracking.swift
@@ -5,4 +5,12 @@
 //  Created by l.barbosa.de.souza on 09/05/26.
 //
 
-import Foundation
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
