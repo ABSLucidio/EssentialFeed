8ff54e58b85adca6755958083a66e3e5a8c3e023
lucidiosouzaavanade <lucidiosouza.avanade@natura.net>
2026-05-09 12:32:44 -0300

    implement all scenarios urlsession and organize files

---
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,8 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by l.barbosa.de.souza on 09/05/26.
+//
+
+import Foundation
---
+++ EssentialFeedTests/Feed API/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,8 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  EssentialFeedTests
+//
+//  Created by l.barbosa.de.souza on 09/05/26.
+//
+
+import Foundation
