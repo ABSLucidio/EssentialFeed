e0fc7f86c4156d34fae4b0c746c48fb8187ee6a2
lucidiosouzaavanade <lucidiosouza.avanade@natura.net>
2026-05-09 13:23:52 -0300

    add CI scheme

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by l.barbosa.de.souza on 09/05/26.
+//
+
+import Testing
+
+struct EssentialFeedAPIEndToEndTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
